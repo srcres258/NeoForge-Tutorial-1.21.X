3086b4ba8662ac52c78de56741018a048efb23fd
src_resources <src.res.211@gmail.com>
2024-12-29 22:20:03 +0800

    Add an event listener

--- src/main/java/top/srcres258/tutorialmod/event/ModEvents.java
+++ src/main/java/top/srcres258/tutorialmod/event/ModEvents.java
@@ -1,9 +1,16 @@
 package top.srcres258.tutorialmod.event;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.animal.Sheep;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Items;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.entity.living.LivingDamageEvent;
 import net.neoforged.neoforge.event.level.BlockEvent;
 import top.srcres258.tutorialmod.TutorialMod;
 import top.srcres258.tutorialmod.item.custom.HammerItem;
@@ -36,6 +43,18 @@ public class ModEvents {
                 HARVESTED_BLOCKS.add(pos);
                 serverPlayer.gameMode.destroyBlock(pos);
                 HARVESTED_BLOCKS.remove(pos);
+            }
+        }
+    }
+
+    @SubscribeEvent
+    public static void livingDamage(LivingDamageEvent.Pre event) {
+        if (event.getEntity() instanceof Sheep sheep
+                && event.getSource().getDirectEntity() instanceof Player player) {
+            if (player.getMainHandItem().getItem() == Items.END_ROD) {
+                player.sendSystemMessage(Component.literal(player.getName().getString() + " just hit a sheep with an END ROD? YOU SICK FRICK!"));
+                sheep.addEffect(new MobEffectInstance(MobEffects.POISON, 600, 6));
+                player.getMainHandItem().shrink(1);
             }
         }
     }
